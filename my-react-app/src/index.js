4883a7801a62b853b23f8250ca319bd4432fe3e6
saladpie23 <129929114+saladpie23@users.noreply.github.com>
2024-10-25 16:06:00 +0900

    241025-1

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // (선택사항) CSS 파일, 필요 없다면 생략 가능합니다.
-import App from './App'; // App 컴포넌트를 불러옵니다.
+import './index.css';
+import App from './App';  // App 컴포넌트를 불러옵니다
 
-// ReactDOM.render() 함수로 App 컴포넌트를 index.html의 root div에 렌더링합니다
 ReactDOM.render(
-  <React.StrictMode>
-    <App />  {/* App 컴포넌트가 여기서 렌더링됩니다 */}
-  </React.StrictMode>,
-  document.getElementById('root')  // public/index.html의 <div id="root"></div>에 렌더링
+    <React.StrictMode>
+        <App />  {/* 메인 컴포넌트로 App 렌더링 */}
+    </React.StrictMode>,
+    document.getElementById('root')  // index.html의 root 요소에 주입
 );
