1a3c9c6863200630efb4f0871774ea3ed705e4ad
saladpie23 <129929114+saladpie23@users.noreply.github.com>
2024-10-25 14:29:58 +0900

    .

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './index.css'; // (선택사항) CSS 파일, 필요 없다면 생략 가능합니다.
+import App from './App'; // App 컴포넌트를 불러옵니다.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// ReactDOM.render() 함수로 App 컴포넌트를 index.html의 root div에 렌더링합니다
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App />  {/* App 컴포넌트가 여기서 렌더링됩니다 */}
+  </React.StrictMode>,
+  document.getElementById('root')  // public/index.html의 <div id="root"></div>에 렌더링
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
